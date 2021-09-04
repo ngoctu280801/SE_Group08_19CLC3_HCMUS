322a6de0f46389e64a6063288e3073d60b356d84
nguyenbaoduy1612001 <nguyenbaoduy1612001@gmail.com>
2021-09-04 09:24:12 +0700

    Update manage_product_view

--- htdocs/application/views/manage_product_view.php
+++ htdocs/application/views/manage_product_view.php
@@ -3,20 +3,22 @@
     
 <!-- login-register31:27-->
 <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Manage Your Products || Group 8</title>
-        <?php include 'head_tag.php';?>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Manage Your Products || Group 8</title>
+    <?php include 'head_tag.php';?>
 </head>
+
 <body>
 <!--[if lt IE 8]>
-	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
     <!-- Begin Body Wrapper -->
     <div class="body-wrapper">
         <!-- Begin Header Area -->
         <?php include 'header.php';?>
         <!-- Header Area End Here -->
+
         <!-- Begin Li's Breadcrumb Area -->
         <div class="breadcrumb-area">
             <div class="container">
@@ -49,32 +51,44 @@
             <h2>Product</h2>
             <button class="shop-information-add-button"><a href="toAddProduct">Add new product</button></a>
             <div class="shops-products-list-container">
-                <ul class="shops-products-list-all">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="li-product-thumbnail">Product Images</th>
+                            <th class="cart-product-name">Name</th>
+                            <th class="cart-product-price">Price</th>
+                            <th class="quantity">Quantity</th>
+                            <th class="li-product-edit">Edit</th>
+                            <th class="li-product-remove">Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    
                     <?php 
-
                     if($info){
                         $total = count($info);
                         for ($i = 0; $i < $total; $i++) { ?>
-                        <li class="shops-products-list-single">
-                            <ul>
-                                <li><img src="<?= base_url().'uploads/'.$img[$i] ?>" alt="Product Icon" ></li>
-                                <li><a href="Product/<?= $info[$i]['id'] ?>"><?= $info[$i]['name'] ?></a></li>
-                                <li><?= $info[$i]['price'] ?>VNĐ</li>
-                                <li input type="button" onclick="location.href='ChangeProductInfo/<?= $info[$i]['id'] ?>'"><i class="fa fa-edit"></i></li>
-                                <li input type="button" onclick="return deleteProduct(<?= $info[$i]['id'] ?>)"><i class="fa fa-trash" ></i></li>
-                                    <script type="text/javascript">
-                                        function deleteProduct(id_product)
-                                        {
-                                            if(confirm("Are you sure want to delete this product ?"))
-                                            {
-                                                return location.href='DeleteProduct/'+id_product;
-                                            }
-                                        }
-                                    </script>
-                            </ul> 
-                        </li>
+                        <tr>
+                            <td class="li-product-thumbnail"><a href="Product/<?= $info[$i]['id'] ?>"><img src="<?= base_url().'uploads/'.$img[$i] ?>" alt="Li's Product Image" style="width:150px;height:150px;"></a></td>
+                            <td class="li-product-name"><a href="Product/<?= $info[$i]['id'] ?>"><?= $info[$i]['name'] ?></a></td>
+                            <td class="li-product-price"><span class="amount" style="font-weight:bold"><?= $info[$i]['price'] ?>VNĐ</span></td>
+                            <td class="quantity" style="font-weight:bold"><?= $info[$i]['instock'] ?></td>
+                            <td class="li-product-edit"><i class="fa fa-edit" onclick="location.href='ChangeProductInfo/<?= $info[$i]['id'] ?>'"></i></td>
+                            <td class="li-product-remove"><i class="fa fa-trash" onclick="return deleteProduct(<?= $info[$i]['id'] ?>)"></i></td>
+                        </tr>
                     <?php } }?>
-                </ul>
+
+                    </tbody>
+                    <script>
+                        function deleteProduct(id_product)
+                        {
+                            if(confirm("Are you sure want to delete this product ?"))
+                            {
+                                return location.href='DeleteProduct/'+id_product;
+                            }
+                        }
+                    </script>
+                </table>
             </div>
         </div>
         <!-- Login Content Area End Here -->
